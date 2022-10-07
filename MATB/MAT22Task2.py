884faad43303e1554c37500daa4a48cd9d95beb5
Adib Shaker <neoadib@gmail.com>
2022-10-08 00:10:45 +0200

    Rowitter added

--- MATB/MAT22Task2.py
+++ MATB/MAT22Task2.py
@@ -5,33 +5,43 @@ import matplotlib.pyplot as plt
 
 
 m = np.matrix([[1, 3, 2], [-3, 4, 3], [2, 3, 1]])
-matrix = m
-k = 10
+#matrix = m
+
+#k = 10
 print(m)
-# def calc(matrix,k):
-
-evalue, evector = LA.eig(matrix)
-
-basis = np.squeeze(np.asarray(evector))
-
-u0 = np.array([8, 3, 12])
-
-c = np.linalg.solve(basis, u0)
 
 
-uk = []
-for j in range(1, k+1):
-    coliter = []
-    for i in range(0, len(basis)):
-        n = basis[:, i] * c[i] * evalue[i]**j
-        coliter.append(n)
-    uk.append(coliter)
-uk = np.array(uk)
-#    return uk
+def calc(matrix, k):
+
+    evalue, evector = LA.eig(matrix)
+
+    basis = np.squeeze(np.asarray(evector))
+
+    u0 = np.array([8, 3, 12])
+
+    c = np.linalg.solve(basis, u0)
+
+    uk = []
+    z = []
+    for j in range(1, k+1):
+        colitter = []
+        for i in range(0, len(basis)):
+            n = basis[:, i] * c[i] * evalue[i]**j
+            colitter.append(n)
+        uk.append(colitter)
+    uk = np.array(uk)
+    #    return uk
+    for n in range(0, len(uk)):
+        rowitter = []
+        for m in range(0, len(uk[n])):
+            elsum = uk[n][m, :].sum()
+            rowitter.append(elsum)
+        z.append(rowitter)
+    return np.array(z)
 
 
-#Z = calc(m,20)
-# print(Z)
+Z = calc(m, 20)
+print(Z)
 '''
 ax = plt.axes(projection='3d')
 
